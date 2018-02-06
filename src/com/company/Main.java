490fc12558c01a1cb606b854260f7a7c075efa81
Mike Williams <MikeWillCodes@gmail.com>
2018-02-06 14:33:40 -0600

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,12 @@ import java.util.Random;
 
 public class Main {
 
+    public static final int  MAX_SIZE = 20;
+
     public static void main(String[] args) {
+
+        ArrayList<Integer> absences = absences(MAX_SIZE);
+        System.out.println("# of absences: " + absences);
 
         ArrayList<String> names = initializeNames();
         System.out.println("The names are: " + names);
@@ -14,17 +19,32 @@ public class Main {
         names = shuffle(names);
         System.out.println("Shuffled names: " + names);
 
-        ArrayList<String> anotherArray = anotherArray(20, names);
-        System.out.println(anotherArray);
+        ArrayList<String> anotherArray = anotherArray(MAX_SIZE, names);
+        System.out.println("List of dups: " + anotherArray);
 
-        checker(anotherArray);
+        boolean check = checker(anotherArray, names);
+        System.out.println(check);
+
+        ArrayList<String> perfAttend = perfectAttendance(absences, anotherArray);
+        System.out.println("Students with perfect attendance: " + perfAttend);
+
+        ArrayList<String> failedOut = fe(absences,anotherArray);
+        System.out.println("Students who failed out: " + failedOut);
 
     }
 
-    public static boolean checker (ArrayList<String> array){
-        for (int i = 0; i < array.size(); i++) {
-            String name = array.get(i);
-            if(array.equals(name)){
+    public static ArrayList<Integer> absences(int size){
+        Random rand = new Random();
+        ArrayList<Integer> absences = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            absences.add(rand.nextInt(10));
+        }
+        return absences;
+    }
+
+    public static boolean checker (ArrayList<String> anotherArray, ArrayList<String> names){
+        for (int i = 0; i < anotherArray.size() ; i++) {
+            if (anotherArray.contains(names.get(i))){
                 return true;
             }
         }
@@ -56,5 +76,25 @@ public class Main {
             anotherListOfNames.add(names.get(pick));
         }
         return anotherListOfNames;
+    }
+
+    public static ArrayList<String> perfectAttendance(ArrayList<Integer> absences, ArrayList<String> names){
+        ArrayList<String> namesWithPerfectAttendance = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++){
+            if(absences.get(i) == 0){
+                namesWithPerfectAttendance.add(names.get(i));
+            }
+        }
+        return namesWithPerfectAttendance;
+    }
+
+    public static ArrayList<String> fe (ArrayList<Integer> absences, ArrayList<String> names){
+        ArrayList<String> namesThatFe= new ArrayList<>();
+        for (int i = 0; i < names.size(); i++){
+            if(absences.get(i) >= 6){
+                namesThatFe.add(names.get(i));
+            }
+        }
+        return namesThatFe;
     }
 }
